757805cc4a98321cf4b11c85b2634b54de440a2f
AlgoClassWork <kyxec.play@gmail.com>
2025-04-06 12:23:34 +0600

    Add files via upload

--- store/models.py
+++ store/models.py
@@ -5,6 +5,9 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
+    def __str__(self):
+        return self.name
+
 
 #python manage.py makemigrations
 #python manage.py migrate
--- store/views.py
+++ store/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+from store.models import Product
+
 # Create your views here.
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'store/product_list.html', {'products': products})
+
