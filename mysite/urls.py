971f8bab6bcea9326d31acacf37d15a64e1a5a49
AlgoClassWork <kyxec.play@gmail.com>
2025-04-13 13:58:09 +0600

    Add files via upload

--- mysite/urls.py
+++ mysite/urls.py
@@ -23,4 +23,6 @@ urlpatterns = [
     path('', views.product_list, name='product_list'),
     path('product/<int:product_id>/', views.product_detail, name='product_detail'),
     path('register/', views.register, name='register'),
+    path('cart/add/<int:product_id>', views.cart_add, name='cart_add'),
+    path('cart/', views.cart_detail, name='cart_detail'),
 ]
